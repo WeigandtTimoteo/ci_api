96c94369600a4d34c70ad92e1d2e4db4d151d000
Weigandt Timoteo <weigandttimoteo@gmail.com>
2026-05-28 11:33:28 -0300

    fix del fetch en vercel

--- config/settings.py
+++ config/settings.py
@@ -54,9 +54,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,9 +134,11 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 CORS_ALLOWED_ORIGINS = [
+    "https://cabanillas-inmobiliaria.vercel.app",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
